16c7501da6e39284900c73061afc7718d37b7b19
rebecca-butler <rebutler@uwaterloo.ca>
2020-08-06 20:38:20 -0400

    Used GoogleMapsPlotter API to draw locations on map

---
+++ README.md
@@ -0,0 +1,5 @@
+# Set-up instructions
+
+pip install argparse pandas xlrd gmplot
+
+usage: python create_map.py ~/Projects/testing/Member_info.xlsx
--- create_map.py
+++ create_map.py
@@ -1,14 +1,24 @@
 import os
 import argparse
 import pandas as pd
+import gmplot
 
 parser = argparse.ArgumentParser(description="Plot addresses from Excel file in Google Maps")
 parser.add_argument("file_path", help="Path to Excel file")
 args = parser.parse_args()
 
+apikey = 'AIzaSyCxK71WdY85AfLFlP1VJlJyXfyKGd84HhI'
+
 df = pd.read_excel(args.file_path)
-'''print (df.columns[0])
-col_one_list = df['Address'].tolist()
-print (col_one_list)'''
+
+# Convert address to lat/long and plot
 for i in df.index:
-    print (df["Address"][i])
+    addr = df["Address"][i]
+    city = df["City"][i]
+    province = df["State"][i]
+    geocode = gmplot.GoogleMapPlotter.geocode('{}, {}, {}'.format(addr, city, province), apikey=apikey)
+    gmap = gmplot.GoogleMapPlotter(geocode[0], geocode[1], 13, apikey=apikey)
+    break
+
+# Draw the map:
+gmap.draw('map.html')
