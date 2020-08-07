38fc58176082e706c28653a28d53adde0b3b9f5d
rebecca-butler <rebutler@uwaterloo.ca>
2020-08-07 19:54:46 -0400

    Added http server for html map

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 pip install argparse pandas xlrd gmplot
 
 usage: python create_map.py ~/Projects/testing/Member_info.xlsx
+
+go to localhost:8888/map.html in browser
--- create_map.py
+++ create_map.py
@@ -2,6 +2,8 @@ import os
 import argparse
 import pandas as pd
 import gmplot
+import http.server
+import socketserver
 
 parser = argparse.ArgumentParser(description="Plot addresses from Excel file in Google Maps")
 parser.add_argument("file_path", help="Path to Excel file")
@@ -11,14 +13,28 @@ apikey = 'AIzaSyCxK71WdY85AfLFlP1VJlJyXfyKGd84HhI'
 
 df = pd.read_excel(args.file_path)
 
-# Convert address to lat/long and plot
+# Convert addresses to lat/long
+lats = []
+longs = []
 for i in df.index:
     addr = df["Address"][i]
     city = df["City"][i]
     province = df["State"][i]
     geocode = gmplot.GoogleMapPlotter.geocode('{}, {}, {}'.format(addr, city, province), apikey=apikey)
-    gmap = gmplot.GoogleMapPlotter(geocode[0], geocode[1], 13, apikey=apikey)
-    break
+    lats.append(geocode[0])
+    longs.append(geocode[1])
 
-# Draw the map:
-gmap.draw('map.html')
+
+# Set center of map and zoom level
+gmap = gmplot.GoogleMapPlotter(43.5503, -79.6914, 9, apikey=apikey)
+
+# Plot locations and draw
+gmap.scatter(lats, longs, "red", size = 10, apikey=apikey)
+gmap.draw("map.html")
+
+# Start server
+port = 8888
+handler = http.server.SimpleHTTPRequestHandler
+print("Serving at port: " + str(port))
+with socketserver.TCPServer(("", port), handler) as httpd:
+    httpd.serve_forever()
