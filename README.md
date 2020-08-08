7b497438886dbfc6c036abda7811471abdbb5a35
rebecca-butler <rebutler@uwaterloo.ca>
2020-08-07 22:02:46 -0400

    Improved command line arguments and added functionality

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 pip install argparse pandas xlrd gmplot geopy
 
-usage: `python create_map.py <file_path> <api_key>`
+usage: `python create_map.py <api_key> <file_path> [-map]`
 
 go to localhost:8888/map.html in browser
--- create_map.py
+++ create_map.py
@@ -5,67 +5,64 @@ import pandas as pd
 import gmplot
 import http.server
 import socketserver
-import time
-from geopy.geocoders import Nominatim 
 from geopy.distance import geodesic
 
-parser = argparse.ArgumentParser(description="Plot addresses from Excel file in Google Maps")
-parser.add_argument("file_path", help="Path to Excel file")
-parser.add_argument("apikey", nargs="?", help="Google Maps API key (optional)")
+parser = argparse.ArgumentParser(description="Cluster addresses in 10km radius")
+parser.add_argument("apikey", nargs="?", help="Google Maps API key")
+parser.add_argument("file_path", nargs="?", help="Path to Excel file (if not provided, stored JSON will be used)")
+parser.add_argument("-map", action="store_true", help="Optional flag to plot locations on map and serve")
 args = parser.parse_args()
 
-df = pd.read_excel(args.file_path)
-'''
-# Convert addresses to lat/long
-lats = []
-longs = []
-locations = []
-for i in df.index:
-    addr = df["Address"][i]
-    city = df["City"][i]
-    province = df["State"][i]
-    geocode = gmplot.GoogleMapPlotter.geocode("{}, {}, {}".format(addr, city, province), apikey=args.apikey)
-    time.sleep(1) # sleep between API calls
-    lats.append(geocode[0])
-    longs.append(geocode[1])
-    locations.append(geocode)
+api_key = args.apikey
 
-# Set center of map and zoom level
-gmap = gmplot.GoogleMapPlotter(43.5503, -79.6914, 9, apikey=args.apikey)
+# If Excel file is given, convert addresses to lat/long
+if args.file_path and args.apikey:
+    df = pd.read_excel(args.file_path)
+    locations = []
+    for i in df.index:
+        addr = df["Address"][i]
+        city = df["City"][i]
+        province = df["State"][i]
+        try:
+            location = gmplot.GoogleMapPlotter.geocode("{}, {}, {}".format(addr, city, province), apikey=api_key)
+            location_obj = {
+                "name": df["Account"][i],
+                "lat": location[0],
+                "long": location[1]
+            }
+            locations.append(location_obj)
+        except:
+            print(addr + " failed")
 
-# Plot locations and draw
-gmap.scatter(lats, longs, "red", size = 10, apikey=args.apikey)
-gmap.draw("map.html")
+    # Store locations in JSON file
+    with open("locations.json", "w") as outfile:
+        json_string = json.dumps(locations, indent=4, sort_keys=True, separators=(',', ': '))
+        outfile.write(json_string)
 
-# Start server
-port = 8888
-handler = http.server.SimpleHTTPRequestHandler
-print("Serving at port: " + str(port))
-with socketserver.TCPServer(("", port), handler) as httpd:
-    httpd.serve_forever()'''
+# Retrieve locations from JSON file
+with open("locations.json") as json_file:
+    location_data = json.load(json_file)
 
-# Convert addresses to lat/long
-locations = []
-for i in df.index:
-    addr = df["Address"][i]
-    city = df["City"][i]
-    province = df["State"][i]
-    geolocator = Nominatim(user_agent="http")
-    location = geolocator.geocode("{}, {}, {}".format(addr, city, province))
-    try:
-        location_obj = {
-            "name": df["Account"][i],
-            "lat": location.latitude,
-            "long": location.longitude
-        }
-        locations.append(location_obj)
-    except:
-        print(addr + " failed")
+# If map flag is set, create map and serve
+if args.map and args.apikey:
+    # Set center of map and zoom level
+    gmap = gmplot.GoogleMapPlotter(43.5503, -79.6914, 9, apikey=args.apikey)
 
-# Store locations
-with open("locations.txt", "w") as outfile:
-    json_string = json.dumps(locations, indent=4, sort_keys=True, separators=(',', ': '))
-    outfile.write(json_string)
+    # Plot locations and draw
+    lats = []
+    longs = []
+    for location in location_data:
+        lats.append(location["lat"])
+        longs.append(location["long"])
+    gmap.scatter(lats, longs, "red", size = 10, apikey=args.apikey)
+    gmap.draw("map.html")
+
+    # Start server
+    port = 8080
+    handler = http.server.SimpleHTTPRequestHandler
+    print("Serving at port: " + str(port))
+    with socketserver.TCPServer(("", port), handler) as httpd:
+        httpd.serve_forever()
 
 # Split locations into set of clusters such that two points within 10km of each other will be in the same cluster
 
